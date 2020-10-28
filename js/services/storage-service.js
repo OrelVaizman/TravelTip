6e3a158af4edc4c0bea2aa5641051ca69234aff9
Orel Vaizman <orelvaizman@h-MacBook-Pro-sl-Orel.local>
2020-10-28 17:53:21 +0200

    idk

--- js/services/storage-service.js
+++ js/services/storage-service.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 function saveToStorage(key, val) {
@@ -9,18 +8,3 @@ function loadFromStorage(key) {
     var val = localStorage.getItem(key)
     return JSON.parse(val)
 }
-=======
-function saveToStorage(key, val) {
-    localStorage.setItem(key, JSON.stringify(val));
-  }
-  
-  function loadFromStorage(key) {
-    const val = localStorage.getItem(key);
-    return JSON.parse(val);
-  }
-
- export const storageService = {
-      saveToStorage,
-      loadFromStorage
-  }
->>>>>>> 74d03ed7df433aea83818cd1862b12fdeaf9449a
