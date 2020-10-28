26e7b0f4bf5e2b75da8a8064293555250ebd3917
Guy Indepurker <guy235@gmail.com>
2020-10-28 18:54:53 +0200

    add locations

--- js/services/location-service.js
+++ js/services/location-service.js
@@ -1,5 +1,7 @@
 console.log('working!!')
 import {storageService} from './storage-service.js'
+import { utilsService } from './utils.js';
+
 let gLocations = loadLocations();
 const LOCATIONS = 'LocationsDB'
 
@@ -8,14 +10,14 @@ function loadLocations(){
     if(!gLocations || !gLocations.length) gLocations = [];
 }
 
-function addLocation(id,name,lat,lng,weather,createdAt,updatedAt=null){
-    gLocations.push(createLocation(id,name,lat,lng,weather,createdAt,updatedAt=null));
+function addLocation(name,lat,lng,weather,createdAt,updatedAt=null){
+    gLocations.push(createLocation(name,lat,lng,weather,createdAt,updatedAt=null));
     storageService.saveToStorage(LOCATIONS,gLocations)   
 }
 
-function createLocation(id,name,lat,lng,weather,createdAt,updatedAt=null){
+function createLocation(name,lat,lng,weather,createdAt,updatedAt=null){
     return {
-        id,
+        id:utilsService.makeId(),
         name,
         lat,
         lng,
